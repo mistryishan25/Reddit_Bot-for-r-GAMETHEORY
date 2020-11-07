18927e0757defc14c81267efaa9c664c562b4f02
mistryishan25 <52032248+mistryishan25@users.noreply.github.com>
2020-11-07 18:31:12 +0530

    coordinates for refernce basically

--- OneDrive/Documents/OSLAB/OS/src/Main.java
+++ OneDrive/Documents/OSLAB/OS/src/Main.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.ClosePath;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
@@ -26,41 +30,81 @@ public class Main extends Application {
         rotate.setPivotX(300);
         rotate.setPivotY(300);
 
-        Arc arc = new Arc();
-        arc.setCenterX(300.0f);
-        arc.setCenterY(300.0f);
-        arc.setRadiusX(160);
-        arc.setRadiusY(160);
+        Line line1 = new Line(140, 300, 460, 300);
+        line1.setStroke(Color.LIGHTGRAY);
+        Line line2 = new Line(300, 140, 300, 460);
+        line2.setStroke(Color.LIGHTGRAY);
+        Line line3 = new Line(300, 140, 300, 460);
+        line3.setStroke(Color.LIGHTGRAY);
+        line3.getTransforms().add(rotate);
+        Line line4 = new Line(140, 300, 460, 300);
+        line4.setStroke(Color.LIGHTGRAY);
+        line4.getTransforms().add(rotate);
 
-        arc.setStartAngle(0);
-        arc.setLength(45);
-        arc.setType(ArcType.ROUND);
-        arc.setStroke(Color.BLACK);
-        root.getChildren().add(arc);
+        root.getChildren().addAll(line1, line2, line3, line4);
+        /*
+         * Arc arc = new Arc(); arc.setCenterX(300.0f); arc.setCenterY(300.0f);
+         * arc.setRadiusX(150); arc.setRadiusY(150);
+         * 
+         * arc.setStartAngle(45); arc.setLength(45); arc.setType(ArcType.ROUND);
+         * arc.setStroke(Color.BLACK); root.getChildren().add(arc);
+         * 
+         * Arc arc2 = new Arc(); arc2.setLength(45); arc2.setStartAngle(45);
+         * arc2.setRadiusX(140); arc2.setRadiusY(140);
+         * 
+         * arc2.setFill(Color.WHITE); arc2.setCenterX(300.0f); arc2.setCenterY(300.0f);
+         * arc2.setType(ArcType.ROUND); arc2.setStroke(Color.LIGHTGRAY);
+         * root.getChildren().add(arc2);
+         */ // THis code is not needed now as we have the code for a path that encloses a
+            // "tile" shape
 
-        Arc arc2 = new Arc();
-        arc2.setLength(45);
-        arc2.setStartAngle(0);
-        arc2.setRadiusX(150);
-        arc2.setRadiusY(150);
-        
-        arc2.setFill(Color.WHITE);
-        arc2.setCenterX(300.0f);
-        arc2.setCenterY(300.0f);
-        arc2.setType(ArcType.ROUND);
-        arc2.setStroke(Color.BLACK);
-        root.getChildren().add(arc2);
+        Path path = new Path();
+        path.setStroke(Color.BLACK);
+
+        MoveTo A = new MoveTo();
+        A.setX(150);
+        A.setY(300);
+
+        LineTo AB = new LineTo();
+        AB.setX(140);
+        AB.setY(300);
+
+        ArcTo BC = new ArcTo();
+        BC.setRadiusX(160);
+        BC.setRadiusY(160);
+        BC.setXAxisRotation(45);
+        BC.setSweepFlag(false);
+        BC.setX(186.86291501015236);
+        BC.setY(413.1370849898476);
+
+        LineTo CD = new LineTo();
+        CD.setY(406.06601717798213);
+        CD.setX(193.93398282201787);
+
+        ArcTo CA = new ArcTo();
+        CA.setRadiusX(150);
+        CA.setRadiusY(150);
+        CA.setXAxisRotation(45);
+        CA.setSweepFlag(true);
+        CA.setX(150);
+        CA.setY(300);
+
+        ClosePath cp = new ClosePath();
+        path.getElements().addAll(A, AB, BC, CD, CA);
+        // path.setFill(Color.BLACK);
+        root.getChildren().addAll(path);
 
         /*
-         * Shape shape = Shape.intersect(arc, arc2); Shape shape2 = Shape.union(arc2,
-         * arc); Shape shape3 = Shape.subtract(shape, shape2); // int - uni Shape shape4
-         * = Shape.subtract(shape2, shape); // uni - int Shape shape5 =
+         * Shape shape = Shape.intersect(arc, arc2); Shape yes = Shape.subtract(arc,
+         * arc2); Shape shape2 = Shape.union(arc2, arc); Shape shape3 =
+         * Shape.subtract(shape, shape2); // int - uni Shape shape4 =
+         * Shape.subtract(shape2, shape); // uni - int Shape shape5 =
          * Shape.subtract(shape4, shape3); Shape shape6 = Shape.subtract(shape5,
          * shape3);
          * 
-         * shape6.setStroke(Color.RED); shape6.setFill(Color.BLACK);
+         * yes.setFill(Color.BLACK);
          * 
-         * root.getChildren().add(shape6);
+         * root.getChildren().add(yes);
          */
         // Shape shape4 = Shape.
         // shape4.setStroke(Color.RED);
@@ -73,35 +117,26 @@ public class Main extends Application {
             circle1.setStroke(Color.BLACK);
             circle1.setStrokeWidth(2);
             root.getChildren().addAll(circle1);
-            /*
-             * for (double j = 45; j <= 315; j += 90) { getting the corodinates of the line
-             * cutting the circle double x_mid = i * Math.cos(j); double y_mid = i *
-             * Math.sin(j); System.out.println("circle with radius  " + i +
-             * "  with line angle intersect at  " + j + "  X =  " + x_mid + "  Y =  " +
-             * y_mid); }
-             */
+
+            for (double j = 0; j <= 315; j += 45) {
+                // getting the corodinates of the line cutting the circle
+                double J = Math.toRadians(j);
+                double x_mid = 300 + (i * Math.cos(J));
+                double y_mid = 300 + (i * Math.sin(J));
+                System.out.println("circle with radius  " + i + "  with line angle intersect at  " + j + "  X =  "
+                        + x_mid + "  Y =  " + y_mid);
+            }
+
         }
 
-        
-          for (int i = 0; i < 8; i++) { Line line1 = new Line(140, 300, 460, 300);
-          line1.setStroke(Color.LIGHTGRAY); Line line2 = new Line(300, 140, 300, 460);
-          line2.setStroke(Color.LIGHTGRAY); Line line3 = new Line(300, 140, 300, 460);
-          line3.setStroke(Color.LIGHTGRAY); line3.getTransforms().add(rotate); Line
-          line4 = new Line(140, 300, 460, 300); line4.setStroke(Color.LIGHTGRAY);
-          line4.getTransforms().add(rotate);
-          
-          root.getChildren().addAll(line1, line2, line3, line4); }
-         
-
         // Creating a scene object
-        Scene scene = new Scene(root, 600, 300);
+        Scene scene = new Scene(root, 800, 500);
         // Setting title to the Stage
         stage.setTitle("Lets get it done");
         // Adding scene to the stage
         stage.setScene(scene);
         // Displaying the contents of the stage
         stage.show();
-
     }
 
     AnimationTimer timer = new AnimationTimer() {
